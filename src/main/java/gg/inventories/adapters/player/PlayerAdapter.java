686a50012a6aac78a7f8a79e06d1b184f2d36113
Xwaffle <chasecool56@gmail.com>
2021-12-14 01:29:12 -0500

    Rename jar to spigot

--- src/main/java/gg/inventories/adapters/player/PlayerAdapter.java
+++ src/main/java/gg/inventories/adapters/player/PlayerAdapter.java
@@ -3,11 +3,7 @@ package gg.inventories.adapters.player;
 import com.google.gson.JsonObject;
 import gg.inventories.adapters.items.ItemAdapter;
 
-import java.lang.reflect.InvocationTargetException;
-
 public abstract class PlayerAdapter <P,I extends ItemAdapter> {
-
-    private I itemAdapter;
 
     public abstract JsonObject toJson(P player);
 
